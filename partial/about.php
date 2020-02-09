e3e7917a41d1256f78e92cbfc2a52ffc8cc34be4
joselilove <macayananjoselin@gmail.com>
2020-02-09 19:20:17 +0800

    setup

---
+++ partial/about.php
@@ -0,0 +1,9 @@
+<section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+    <div class="w-100">
+        <h1 class="mb-0">Macayanan, Joselin T.</h1>
+        <h5 class="mt-5">Objectives</h5>
+        <p class="lead mb-5">To be part of company that indulges professional growth which provides challenging and rewarding career while allowing me to utilize my knowledge and skills.</p>
+    </div>
+</section>
+
+<hr class="m-0">
---
+++ partial/contact.php
@@ -0,0 +1,19 @@
+<section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="contact">
+    <div class="w-100">
+        <h2 class="mb-5">Contact</h2>
+        <div class="subheading mb-5">1474 Galvani Street, Brgy San Isidro, Makati City
+            <br>
+            <a href="tel:09069279485">09069279485</a>
+            <br>
+            <a href="mailto:macayananjoselin@gmail.com">macayananjoselin@gmail.com</a>
+        </div>
+        <div class="social-icons">
+            <a href="https://github.com/joselilove">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="https://www.facebook.com/jonh.macayanan">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+        </div>
+    </div>
+</section>
